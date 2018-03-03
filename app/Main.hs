93e04648300497c2369d445e7c8216c72b6d3ce0
David Wagner <wagdav@gmail.com>
2018-03-03 12:03:20 +0100

    Add benchmark set.

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,15 @@
 module Main where
 
+import Criterion.Main
 import Lib
 
+changeAmount amount = change amount 4
+
 main :: IO ()
-main = undefined
+main = defaultMain [
+    bgroup "min-coin-change" [ bench "40"  $ whnf changeAmount 40
+                             , bench "100" $ whnf changeAmount 100
+                             , bench "150" $ whnf changeAmount 150
+                             , bench "200" $ whnf changeAmount 200
+                             ]
+    ]

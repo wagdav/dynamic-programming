b8bdc75498ddf58c5508b3d2088f971d88af5a5a
David Wagner <wagdav@gmail.com>
2018-03-04 20:13:25 +0100

    Split test suite.

--- test/Spec.hs
+++ test/Spec.hs
@@ -5,13 +5,22 @@ import Test.Hspec
 import qualified Dynamic
 import qualified Naive
 
-spec = describe "Min Coin Exchange" $ do
-    it "solves the example" $
+specNaive = describe "Min Coin Exchange (naive implementation)" $ do
+    it "40 = 2 x 20" $
        Naive.change 40 4 `shouldBe` Just 2
 
-    it "solves the example" $
+    it "13 = 10 + 3 x 1" $
+       Naive.change 13 4 `shouldBe` Just 4
+
+specDynamic = describe "Min Coin Exchange (dynamic programming)" $ do
+    it "40 = 2 x 20" $
        Dynamic.change 40 4 `shouldBe` Just 2
+
+    it "13 = 10 + 3 x 1" $
+       Dynamic.change 13 4 `shouldBe` Just 4
+
 
 main :: IO ()
 main = hspec $ do
-    spec
+    specDynamic
+    specNaive

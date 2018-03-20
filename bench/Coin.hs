fc03fc86cc3873e282097ad09fd60f0129c0d6d1
David Wagner <wagdav@gmail.com>
2018-03-20 21:27:10 +0100

    Move benchmark code under bench/

---
+++ bench/Coin.hs
@@ -0,0 +1,22 @@
+module Main where
+
+import Criterion.Main
+import qualified Coin.Dynamic
+import qualified Coin.Naive
+
+changeNaive amount = Coin.Naive.change amount 4
+changeDynamic amount = Coin.Dynamic.change amount 4
+
+main :: IO ()
+main = defaultMain
+    [ bgroup "naive" [ bench "40"  $ whnf changeNaive 40
+                     , bench "100" $ whnf changeNaive 100
+                     , bench "150" $ whnf changeNaive 150
+                     , bench "200" $ whnf changeNaive 200
+                     ]
+    , bgroup "dynamic" [ bench "40"  $ whnf changeDynamic 40
+                       , bench "100" $ whnf changeDynamic 100
+                       , bench "150" $ whnf changeDynamic 150
+                       , bench "200" $ whnf changeDynamic 200
+                       ]
+    ]
